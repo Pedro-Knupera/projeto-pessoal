69530f337d412216e239aa18dc3988a5b493a976
Pedro <Pedro@sg.srv>
2021-02-18 19:36:04 -0300

    Fiz algumas modificações em functions.JS, index.html da pagina principal, index.html da pasta Solicitações.

--- functions.js
+++ functions.js
@@ -1,4 +1,11 @@
-function HideMenu(){
-document.getElementsByClassName('menu-flutuante')[0].style.visibility = 'hidden';
-document.getElementsByClassName('menu-flutuante')[0].style.height = '0px';
-document.getElementsByClassName('logo-img')[0].style.position = 'relative';}
+function HideMenu() {
+    document.getElementsByClassName('menu-flutuante')[0].style.visibility = 'hidden';
+    document.getElementsByClassName('menu-flutuante')[0].style.height = '0px';
+    document.getElementsByClassName('logo-img')[0].style.position = 'relative';
+}
+function changecolorfollow() {
+     if(true) { document.getElementById('buttonseguir').style.backgroundColor= "blue"
+     document.getElementById('buttonseguir').innerHTML="Seguindo"
+    }
+}
+
