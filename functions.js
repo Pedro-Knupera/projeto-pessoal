f1cceb30c22af2ce644b866e774e592a81c72b18
Pedro <Pedro@sg.srv>
2021-02-18 16:09:10 -0300

    Adição da pasta imagens e do arquivo .JS na pagina principal e na pasta solucoes

---
+++ functions.js
@@ -0,0 +1,4 @@
+function HideMenu(){
+document.getElementsByClassName('menu-flutuante')[0].style.visibility = 'hidden';
+document.getElementsByClassName('menu-flutuante')[0].style.height = '0px';
+document.getElementsByClassName('logo-img')[0].style.position = 'relative';}
