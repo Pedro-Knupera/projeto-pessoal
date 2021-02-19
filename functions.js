230263ae53f908f16ad83f527fe46a1846f82d5f
Pedro <Pedro@sg.srv>
2021-02-19 20:33:22 -0300

    Alterações no javaScript

--- functions.js
+++ functions.js
@@ -3,6 +3,7 @@ function HideMenu() {
     document.getElementsByClassName('menu-flutuante')[0].style.height = '0px';
     document.getElementsByClassName('logo-img')[0].style.position = 'relative';
 }
+
 function ButtonSeguirCgC(){
     document.getElementById('buttonseguir').style.backgroundColor = "#C579E5"
     document.getElementById('buttonseguir').innerHTML = "Seguindo"
